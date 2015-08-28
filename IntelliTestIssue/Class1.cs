6afc802cafce3d8772b59dad3309eccdf71a8090
Cayde Dixon <me@cazzar.net>
2015-08-28 16:11:03 +1000

    Initial Comit

---
+++ IntelliTestIssue/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliTestIssue
+{
+    class Class1
+    {
+        public string Bas64Encode(string input) => Convert.ToBase64String(Encoding.UTF8.GetBytes(input)); //This is fine
+        public string Base64Decode(string input) => Encoding.UTF8.GetString(Convert.FromBase64String(input)); //I would expect more than one just to pass.
+    }
+}
